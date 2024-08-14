95a3fbabcd0064ce2eacc50a825f9c9d1e66bd63
jikho7 <defayes.julie@gmail.com>
2024-08-14 12:51:37 +0200

    handling char

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -1,100 +1,196 @@
 #include "ScalarConverter.hpp"
    
-    ScalarConverter::ScalarConverter() : _isPseudoLiteral(false), _isInputValid(false){}
+    ScalarConverter::ScalarConverter() : _type(0){}
     ScalarConverter::~ScalarConverter(){}
-    ScalarConverter::ScalarConverter(const ScalarConverter &other)
-    {
-        *this = other;
-    }
+
+    ScalarConverter::ScalarConverter(const ScalarConverter &other){
+        *this = other;}
 
     ScalarConverter& ScalarConverter::operator=(const ScalarConverter &other)
     {
-        if (this != &other)
-        {
-            this->_isInputValid = other._isInputValid;
-            this->_isPseudoLiteral = other._isPseudoLiteral;
-        }
+        if (this != &other){}
         return *this;
     }
 
     void ScalarConverter::convert(std::string literalString)
     {
         ScalarConverter converter;
-        int intResult = 0;
-        float floatResult = 0;
-        double doubleResult = 0;
-        char charResult;
-        const char *str = literalString.c_str();
 
-        if(converter.isPseudoLiteral(literalString))
-        {
-            doubleResult =  std::atof(str); 
-            floatResult =  static_cast<float>(doubleResult);
-        }
-        else if (converter.isInputValid(literalString))
-        {
-            doubleResult =  std::atof(str); 
-            intResult = static_cast<int>(doubleResult);
-            charResult = intResult;
-            floatResult =  static_cast<float>(doubleResult);
+        converter.findType(literalString);
 
+        if((converter.getType() != PSEUDOLITERALNAN && converter.getType() != PSEUDOLITERALINF) && converter.isInputValid(literalString))
+        {
+            converter.convertFromStringToType(literalString);
+            converter.explicitConversion();
+            converter.printOutput(literalString);
         }
-        converter.printOutput(str, charResult, intResult, floatResult, doubleResult);
+        if((converter.getType() == PSEUDOLITERALNAN || converter.getType() == PSEUDOLITERALINF))
+        {
+            converter.printOutput(literalString);
+        }
     }
 
-    bool ScalarConverter::isInputValid(std::string literalString)
-    {               
-        int isDouble = 0;
-        int isFloat = 0;
-        
-        for (std::string::iterator it = literalString.begin(); it != literalString.end(); ++it)
-        {
-            if (*it == '.')
-                isDouble++;
-            if (*it == 'f')
-                isFloat++;
-            if ((!isdigit(*it) && *it != '.' && *it != 'f' && *it != '-' ) || isDouble > 1 || isFloat > 1)
-            {
-                return (false);
-            }
-        }
-        this->_isInputValid = true;
-        return (true);
-    }
-
-    void ScalarConverter::printOutput(const char *str, char charResult, int intResult, float floatResult, double doubleResult)
+    void ScalarConverter::findType(std::string literalString)
     {
-        if (this->_isPseudoLiteral)
+        this->_type = INT;
+        if (!isPseudoLiteral(literalString))
         {
-            std::cout << "char: impossible" << std::endl;
-            std::cout << "int: impossible" << std::endl;
-            std::cout << "float: " << floatResult << "f" << std::endl;
-            std::cout << "double: " << doubleResult << std::endl;
-        }
-        else if (this->_isInputValid)
-        {
-            if (intResult > 3 && intResult < 127)
-                std::cout << "char: " << charResult << std::endl;
-            else
+            for (std::string::iterator it = literalString.begin(); it != literalString.end(); ++it)
             {
-                std::cout << "char: No displayable" << std::endl;
+                if (*it == '.')
+                {
+                    this->_type = DOUBLE;
+                }
+                if (*it == 'f' && isdigit(*(it - 1 )))
+                {
+                    this->_type = FLOAT;
+                }
+                else if (isalpha(*it))
+                    this->_type = CHAR;
             }
-            std::cout << "int: " << intResult << std::endl;
-            std::cout << "float: " << floatResult << ".0f" << std::endl;
-            std::cout << "double: " << doubleResult << ".0" << std::endl;
         }
+        std::cout << "type : " << this->_type << std::endl;
+    }
+
+bool ScalarConverter::isInputValid(std::string literalString)
+{
+    int isDouble = 0;
+    int isFloat = 0;
+    bool hasDecimal = false;
+    bool hasDigitAfterDecimal = false;
+
+    for (std::string::iterator it = literalString.begin(); it != literalString.end(); ++it)
+    {
+        if (std::isalpha(*it) && (it + 1) == literalString.end())
+            return true;
+        if (*it == '.')
+        {
+            isDouble++;
+            hasDecimal = true;
+        }
+        else if (hasDecimal && isdigit(*it))
+        {
+            hasDigitAfterDecimal = true;
+        }
+        if (*it == 'f')
+        {
+            isFloat++;
+            if (it != literalString.end() - 1 || !hasDecimal || !hasDigitAfterDecimal)
+            {
+                std::cerr << "Input is not valid.." << std::endl;
+                return false;
+            }
+        }
+        if ((*it != '-') && (isDouble > 1 || isFloat > 1))
+        {
+            std::cerr << "Input is not valid." << std::endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+    void ScalarConverter::printOutput(std::string literalString)
+    {
+        if (isPseudoLiteral(literalString))
+            ScalarConverter::printPseudoLiteral(literalString);
         else
-            std::cerr << "Input not valid: " << str << std::endl;
+        {
+            if (isprint(this->_charResult))
+                std::cout << "char: " << this->_charResult << std::endl;
+            else
+                std::cout << "char: No displayable" << std::endl;
+            std::cout << "int: " << this->_intResult << std::endl;
+            if (this->_floatResult == static_cast<int>(this->_floatResult))
+                std::cout << "float: " << this->_floatResult << ".0f" << std::endl;
+            else
+                std::cout << "float: " << this->_floatResult << "f" << std::endl;
+
+            if (this->_doubleResult == static_cast<int>(this->_doubleResult))
+                std::cout << "double: " << this->_doubleResult << ".0" << std::endl;
+            else
+                std::cout << "double: " << this->_doubleResult << std::endl;
+        }
+    }
+
+
+    void ScalarConverter::convertFromStringToType(std::string literalString)
+    {
+        const char *str = literalString.c_str();
+ 
+        switch(this->_type)
+        {
+            case(INT):
+                this->_intResult = std::atoi(str);
+                break;
+            case(FLOAT):
+                this->_floatResult =  std::atof(str);
+                break;
+            case(DOUBLE):
+                this->_doubleResult =  std::strtod(str, NULL);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void ScalarConverter::explicitConversion()
+    {
+        switch(this->_type)
+        {
+            case(INT):
+                this->_floatResult = static_cast<float>(this->_intResult);
+                this->_charResult = this->_intResult;
+                this->_doubleResult =  static_cast<double>(this->_intResult);
+                break;
+            case(FLOAT):
+                this->_intResult = static_cast<int>(this->_floatResult);
+                this->_doubleResult = static_cast<double>(this->_floatResult);
+                this->_charResult = this->_intResult;
+                break;
+            case(DOUBLE):
+                this->_intResult =  static_cast<double>(this->_doubleResult);
+                this->_floatResult = static_cast<float>(this->_doubleResult);
+                this->_charResult = this->_intResult;
+            default:
+                break;
+        }
     }
 
     bool ScalarConverter::isPseudoLiteral(std::string literalString)
     {
-        if (literalString == "nan" || literalString == "nanf" || literalString == "-inff" || literalString == "+inff" 
+        if (literalString == "nan" || literalString == "nanf")
+        {
+            this->_type = PSEUDOLITERALNAN;
+            return true;
+        }
+        else if (literalString == "-inff" || literalString == "+inff" 
             || literalString == "-inf" || literalString == "+inf")
         {
-            this->_isPseudoLiteral = true;
-            return (true);
+            this->_type = PSEUDOLITERALINF;
+            return true;
         }
-        else
-            return (false);
+        return false;
+    }
+
+    void ScalarConverter::printPseudoLiteral(std::string literalString)
+    {
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+
+        if (this->_type == PSEUDOLITERALNAN)
+        {
+            std::cout << "float: nanf" << std::endl;
+            std::cout << "double: nan" << std::endl;
+        }
+        if (this->_type == PSEUDOLITERALINF)
+        {
+            std::cout << "float: " << literalString << std::endl;
+            std::cout << "double: " << literalString << std::endl;
+        }
+    }
+
+    int ScalarConverter::getType()const
+    {
+        return (this->_type);
     }
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -7,10 +7,22 @@
 #include <iostream>
 #include <ostream>
 
+#define INT 0
+#define FLOAT 1
+#define DOUBLE 2
+#define PSEUDOLITERALNAN 3
+#define PSEUDOLITERALINF 4
+#define CHAR 5
+
+
 class ScalarConverter
 {
-        bool _isPseudoLiteral;
-        bool _isInputValid;
+        int     _type;
+        int     _intResult;
+        float   _floatResult;
+        double  _doubleResult;
+        char    _charResult;
+
         ScalarConverter();
         ~ScalarConverter();
 
@@ -19,10 +31,15 @@ class ScalarConverter
         ScalarConverter &operator=(const ScalarConverter &other);
 
         static void convert(std::string literalString);
-        bool isInputValid(std::string literalString);
-        void printOutput(const char *str, char charResult, int intResult, float floatResult, double doubleResult);
-        bool isPseudoLiteral(std::string literalString);
+        bool        isInputValid(std::string literalString);
+        void        printOutput(std::string literalString);
+        void        printPseudoLiteral(std::string literalString);
 
+        bool        isPseudoLiteral(std::string literalString);
+        void        findType(std:: string literalString);
+        void        convertFromStringToType(std::string literalString);
+        void        explicitConversion();
+        int         getType()const;
 };
 
 
